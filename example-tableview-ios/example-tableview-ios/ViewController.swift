a41e354e0db57d4317dcd686d4cbf8bdb2344819
Ricardo Granja <chirica2501@hotmail.com>
2020-09-09 19:33:46 -0500

    Init project

---
+++ example-tableview-ios/example-tableview-ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  example-tableview-ios
+//
+//  Created by Ricardo Granja on 09/09/20.
+//  Copyright © 2020 Ricardo Granja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
